d863453d3cb359aefa590d2feab0832eaa4e07d9
masalzr2 <masalzr2@illinois.edu>
2019-08-10 14:21:18 -0500

    Implemented and completed UoroborosList
    Needs testing and possible debugging. I would also like to fix up the
    negative index behavior to not need the while loop but Java handles
    negative modulars differently

---
+++ Version7/src/main/java/monopoly7/pathFinding/MonopolyExplorable.java
@@ -0,0 +1,17 @@
+package monopoly7.pathFinding;
+
+import java.awt.geom.Point2D.Double;
+import java.util.List;
+
+public class MonopolyExplorable implements Explorable {
+
+	
+	
+	
+	@Override
+	public List<Double> getReachablePoints(double characteristic, double point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
